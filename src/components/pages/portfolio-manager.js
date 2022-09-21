0b54dffcc6c51c043ad355dd0fa245fb0c810f5e
PhilipRoberts <linksbane@gmail.com>
2022-09-21 14:39:19 -0400

    commit 2

---
+++ src/components/pages/portfolio-manager.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+import PortfolioSidebarList from '../portfolio/portfolio-sidebar-list';
+import PortfolioForm from '../portfolio/portfolio-form';
+
+export default class PortfolioManager extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            portfolioItems: []
+        };
+
+        this.handleSuccessfulFormSubmission = this.handleSuccessfulFormSubmission.bind(this);
+        this.handleFormSubmissionError = this.handleFormSubmissionError.bind(this);
+        this.handleDeleteClick = this.handleDeleteClick.bind(this);
+    }
+
+    handleDeleteClick(portfolioItem) {
+        console.log("Delete Click", portfolioItem);
+    }
+
+    handleSuccessfulFormSubmission(portfolioItem) {
+        this.setState({
+            portfolioItems: [portfolioItem].concat(this.state.portfolioItems)
+        });
+    }
+
+    handleFormSubmissionError(error) {
+        console.log("handleFormSubmissionError", error);
+    }
+
+    getPortfolioItems() {
+        axios.get("https://philiproberts.devcamp.space/portfolio/portfolio_items?order_by=created_at&direction=desc", {
+            withCredentials: true
+        }).then(response => {
+            this.setState({
+                portfolioItems: [...response.data.portfolio_items]
+            });
+        }).catch(error => {
+            console.log("error in getPortfolioItems", error);
+        });
+    }
+
+    componentDidMount() {
+        this.getPortfolioItems();
+    }
+
+    render() {
+        return (
+            <div className='portfolio-manager-wrapper'>
+                <div className='left-column'>
+                    <PortfolioForm
+                        handleSuccessfulFormSubmission={this.handleSuccessfulFormSubmission}
+                        handleFormSubmissionError={this.handleFormSubmissionError}
+                    />
+                </div>
+                <div className='right-column'>
+                    <PortfolioSidebarList
+                        handleDeleteClick={this.handleDeleteClick}
+                        data={this.state.portfolioItems}
+                    />
+                </div>
+            </div>
+        );
+    }
+}
